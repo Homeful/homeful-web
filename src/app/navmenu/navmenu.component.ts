89e13237a1a8d81a29b239b338b968c611c4cb72
Conner Bush <connerbush@gmail.com>
2018-08-24 09:11:41 -0500

    makes navbar toggle on click and moves navigate buttons to the right

--- src/app/navmenu/navmenu.component.ts
+++ src/app/navmenu/navmenu.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component } from "@angular/core";
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'navmenu',
-  templateUrl: './navmenu.component.html',
-  styleUrls: ['./navmenu.component.css']
+  selector: "navmenu",
+  templateUrl: "./navmenu.component.html",
+  styleUrls: ["./navmenu.component.css"]
 })
 export class NavmenuComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset: boolean;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches)
+      map(result => {
+        this.isHandset = result.matches;
+        return result.matches;
+      })
     );
-    
+
   constructor(private breakpointObserver: BreakpointObserver) {}
-  
+
+  closeNav(drawer: any) {
+    if (this.isHandset) drawer.toggle();
   }
+}
